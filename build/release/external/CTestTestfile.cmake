30fe5efc122131656511bad71be3712334a18a2d
root <root@LAPTOP-KE1HU12J.localdomain>
2018-05-24 22:26:32 +0300

    deploy

--- build/release/external/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/warranty/external
-# Build directory: /home/warranty/build/release/external
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs(miniupnpc)
-subdirs(gtest)
--- build/release/external/gtest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/warranty/external/gtest
-# Build directory: /home/warranty/build/release/external/gtest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
